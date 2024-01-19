ec977f9f51c923aa75007d82a9c6d901e4a0365b
arekstasko <arek.stasko@gmail.com>
2024-01-19 22:57:25 +0100

    Add new directory

--- CodeWars/obj/Debug/net6.0/CodeWars.AssemblyInfo.cs
+++ CodeWars/obj/Debug/net6.0/CodeWars.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeWars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217926b7357a7918d186cfcd1905497924064908")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e9e148079b16338aac51d4ce29fdb1977342cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeWars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeWars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
