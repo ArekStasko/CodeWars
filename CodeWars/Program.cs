fa172c596fb60d6f8190fb0f6890a4ef1c27beed
arekstasko <arek.stasko@gmail.com>
2024-01-22 18:59:08 +0100

    Add bestMatch algo

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-using CodeWars.LastDigit;
-using CodeWars.MeanSquare;
+﻿using CodeWars.bestMatch;
 
-Console.WriteLine("Should be: 16.5");
-Console.WriteLine(MeanSquare.Solution(new int[] {10, 20, 10, 2}, new int[] {10, 25, 5, -2}));
+Console.WriteLine("Should be: 1");
+Console.WriteLine(BestMatchTest.BestMatch(new int[]{20, 5, 12, 11, 9, 10, 5, 11, 10, 17, 1, 9, 8, 14}, new int[]{10, 3, 5, 3, 4, 8, 4, 3, 1, 9, 0, 2, 3, 4}));
---
+++ CodeWars/bestMatch/BestMatch.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeWars.bestMatch;
+
+public class BestMatchTest
+{
+    public static int BestMatch(int[] goals1, int[] goals2)
+    {
+        int result = int.MaxValue;
+        int index = 0;
+
+        for (int i = 0; i < goals1.Length; i++)
+        {
+            int difference = Math.Abs(goals1[i] - goals2[i]);
+            if (difference < result)
+            {
+                result = difference;
+                index = i;
+            }
+
+            if (difference == result && goals1[i] > goals2[i] && goals1[i] > goals1[index])
+            {
+                result = difference;
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
